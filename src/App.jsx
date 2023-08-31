e99c63670d5cc300396c0acdf64b2c71b5d58f57
Joel <joellongaresjr@gmail.com>
2023-08-30 23:53:00 -0700

    changed initial component structure, will continue to test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
 import './App.css'
+import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
+
+import { Outlet } from 'react-router-dom';
+
 
 function App() {
   return (
     <>
-      
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App
