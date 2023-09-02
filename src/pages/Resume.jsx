ccff6739b865da8f9ac09c34120e0d7d981c56f5
Joel <joellongaresjr@gmail.com>
2023-09-01 21:02:32 -0700

    finished resume component

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,38 @@
+import React from "react";
+
 const Resume = () => {
   return (
-    <section className='flex justify-center items-center h-screen'>
-      <div className='bg-white border-2 border-[#FCB97D] p-64 mb-64'>
-        <h1 className='text-2xl font-bold mb-4'>Resume</h1>
-      </div>  
+    <section className="flex justify-center items-center h-screen">
+      <div className="bg-white border-2 border-[#FCB97D] p-64 mb-64">
+        <h1 className="text-2xl font-bold mb-4">Resume</h1>
+        <hr />
+
+        <div className="mt-5">
+          <h2 className="top-title">click here to download</h2>
+          <a
+            href="/Resume 2023.pdf"
+            className="link inline-block bg-[#FCB97D] text-white px-4 py-2 rounded-full hover:bg-orange-500"
+            download
+          >
+            Download Resume
+          </a>
+        </div>
+
+        <div className="justify-content-center mt-5">
+          <div>
+            <h2 className="top-title">Proficiencies</h2>
+            <ul>
+              <li>
+                HTML, CSS (Bootstrap, HandleBars), JavaScript, jQuery, React.js,
+                IndexedDB, Node.js, Express.js, SQL, Sequelize, MongoDB,
+                Mongoose, NoSQL, APIs, Templating, GraphQL.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </section>
   );
-};
+}
 
 export default Resume;
